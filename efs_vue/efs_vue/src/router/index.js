22e915d79ce9cbf487108b20d9f4fbc0007d6aee
callahanpilkington <111899960+callahanpilkington@users.noreply.github.com>
2023-04-06 11:48:38 -0500

    commit

--- efs_vue/efs_vue/src/router/index.js
+++ efs_vue/efs_vue/src/router/index.js
@@ -1,25 +1,100 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home.vue'
+import Auth from '@/components/Auth.vue'
+import Register from '@/components/Register'
+import CustomerList from '@/components/CustomerList'
+import CustomerCreate from '@/components/CustomerCreate'
+import InvestmentList from '@/components/InvestmentList'
+import InvestmentCreate from '@/components/InvestmentCreate'
+
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
+  },
+
+  {
+    path: '/customer-list',
+    name: 'CustomerList',
+    component: CustomerList
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/customer-list/:msg',
+    name: 'CustomerUpdatedList',
+    component: CustomerList
+  },
+  {
+    path: '/customer-create',
+    name: 'CustomerCreate',
+    component: CustomerCreate
+  },
+  {
+    path: '/customer-create/:pk',
+    name: 'CustomerUpdate',
+    component: CustomerCreate
+  },
+  {
+    path: '/investment-list',
+    name: 'InvestmentList',
+    component: InvestmentList
+  },
+  {
+    path: '/investment-list/:msg',
+    name: 'InvestmentUpdatedList',
+    component: InvestmentList
+  },
+  {
+    path: '/investment-create',
+    name: 'InvestmentCreate',
+    component: InvestmentCreate
+  },
+  {
+    path: '/investment-create/:pk',
+    name: 'InvestmentUpdate',
+    component: InvestmentCreate
+  },
+  {
+    path: '/stock-list',
+    name: 'StockList',
+    component: StockList
+  },
+{
+    path: '/stock-list/:msg',
+    name: 'StockUpdatedList',
+    component: StockList
+  },
+  {
+    path: '/stock-create',
+    name: 'StockCreate',
+    component: StockCreate
+  },
+  {
+    path: '/stock-create/:pk',
+    name: 'StockUpdate',
+    component: StockCreate
+  },
+
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: Auth
+  },
+  {
+    path:'/register',
+    name: 'Register',
+    component: Register
+  } 
 ]
 
+
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
+
 export default router
+
